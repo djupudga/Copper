bbe9e1d8f22484ef641dac899b2bdb5d5cec1d1c
Helgi Kristjansson <helgi.kristjansson@gmail.com>
2012-11-19 18:13:40 +0100

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,84 @@ Copper
 ======
 
 Copper is a Javascript implementation of Composite Oriented Programming.
+The idea is that instead of declaring types (classes) and using
+inheritance to extend those, you assemble or compose data types
+from other objects or types.
+
+In traditional class based languages such as Java you would do
+something like this:
+´´´java
+public class Person {
+    private String name;
+    public Person(String name) {
+        this.name = name;
+    }
+    public void sayName() {
+        System.out.println("My name is " + name)
+    }
+}
+
+public class Employee extends Person {
+    private int employeeNumber;
+    public Employee(String name, int employeeNumber) {
+        super(name);
+        this.employeeNmber = employeeNumber;
+    }
+    public String sayNumber() {
+        System.out.println("My number is " + employeeNumber);
+    }
+}
+// Usage
+Person p = new Person("John");
+p.sayName(); // "My name is John"
+Employee emp = new Person("George", 123);
+emp.sayName(); // My name is George
+emp.sayNumber(); // My number is 123
+
+´´´
+Whereas in a Composite Oriented Programming system, such as Copper
+you would do this:
+
+´´´javascript
+var personBehaviour = {
+    sayName: function() {
+        console.log('My name is ' + this.name)
+    }
+}
+var employeeBehaviour = {
+    sayNumber: function() {
+        console.log('My number is ' + this.employeeNumber)
+    }
+}
+copper.compose({
+      name: 'Person'
+    , mixins: [
+        personBehaviour
+    ]
+    , create: function(name) {
+        this.name = name
+    }
+})
+copper.compose({
+      name: 'Employee'
+    , mixins: [
+          personBehaviour
+        , employeeBehaviour
+    ]
+    , create: function(name, employeeNumber) {
+        this.name = name
+        this.employeeNumber = employeeNumber
+    }
+})
+var person = copper.create('Person', 'John')
+person.sayName()    // My name is John
+var emp = copper.create('Employee', 'George', 123)
+emp.sayName()       // My name is George
+emp.sayNumber()      // My number is 123
+´´´
+
+Instead of defining a type (a class), you define the behaviour and then
+compose your objects using these behaviours.
+
+
+
---
+++ copper.js
@@ -0,0 +1,198 @@
+(function(context, undefined) {
+    // A registry that contains all compositions
+    // registered by name.
+    var assemblies = {}
+
+    var isArray = Array.isArray || function isArray(obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]'
+    }
+
+    function toArray(obj) {
+        return Array.prototype.slice.call(obj)
+    }
+
+    // Creates a before advice that will execute before the given function
+    // is executed.
+    // Parameters:
+    //
+    // *    advice
+    //      > The advice function
+    //
+    // *    object
+    //      > Object or module containing function to execute by the advice.
+    //
+    // *    functionName
+    //      > Name of function to be executed by the advice
+    //
+    // *    executionContext
+    //      > The execution context of the advice if other than object.
+    //
+    //
+    // ###Example
+    //
+    //      // Calling Model.save() will execute the before advice first
+    //      copper.addBefore(SomeModel, 'save', function(val) {
+    //          if (val == undefined)
+    //              throw new Error('Value can\'t be undefined');
+    //      });
+    function before(advice, object, functionName, executionContext) {
+        if (!object) throw new Error('#addBefore: object can\'t be undefined');
+        var callback = object[functionName];
+        var target = object;
+        if (!callback) {
+            callback = object.prototype[functionName];
+            target = object.prototype;
+        }
+        target[functionName] = function() {
+            var _context = executionContext || this;
+            advice.apply(_context, arguments);
+            return callback.apply(_context, arguments);
+        }
+    }
+    // Creates an after advice on a function that executes after the function
+    // has executed.
+    function after(advice, object, functionName, executionContext) {
+        if (!object) throw new Error('#addAfter: object can\'t be undefined');
+        var callback = object[functionName];
+        var target = object;
+        if (!callback) {
+            callback = object.prototype[functionName];
+            target = object.prototype;
+        }
+        target[functionName] = function() {
+            var _context = executionContext || this;
+            var retVal = callback.apply(_context, arguments);
+            if (!isArray(retVal)) {
+                retVal = Array(retVal);
+            }
+            return advice.apply(_context, retVal);
+        }
+    }
+    // Creates an around advice. The last parameter to the around advice is the
+    // function to embed.
+    //      function transactional(dataToSaveIntoDatabase, yield) {
+    //          try {
+    //              TransactionManager.begin();
+    //              yield(dataToSaveIntoDatabase);
+    //              TransactionManager.commit();
+    //          } catch (e) {
+    //              TransactionManager.rollback();
+    //              throw new TransactionError(
+    //                  "Transaction failed with error: " + e.message
+    //              );
+    //          }
+    //      }
+    function around(advice, object, functionName, executionContext) {
+        var fn
+          , target
+
+        if (!object) throw new Error('#addAround: object can\'t be undefined');
+        fn = object[functionName];
+        target = object;
+        if (!fn) {
+            fn = object.prototype[functionName];
+            target = object.prototype;
+        }
+        target[functionName] = function() {
+            var _context = executionContext || this;
+            var callback = function() {
+                return fn.apply(_context, arguments);
+            };
+            var params = toArray(arguments);
+            params.push(callback);
+            return advice.apply(_context, params);
+        }
+    }
+
+    // Mixin arguments to target.
+    function mixin(target) {
+        var mixins
+        if (isArray(arguments[1])) mixins = arguments[1]
+        else {
+            mixins = toArray(arguments)
+            mixins.shift()
+        }
+        for (var i = 0, len = mixins.length, mix; i < len; i++) {
+            mix = mixins[i]
+            for (var j in mix) {
+                target[j] = mix[j]
+            }
+        }
+        return target
+    }
+
+    // Composes an object based on the provided composition
+    // and returns an instance of the composed object. If name
+    // is provided, the composed object is registered and available
+    // via copper.create(namespace)
+    function compose(composition) {
+        var obj = {}
+
+        if (composition.mixins !== undefined) {
+            mixin(obj, composition.mixins)
+        }
+        var aspects = {
+              before: before
+            , after: after
+            , around: around
+        }
+        function processAspects(name) {
+            if (composition[name]) {
+                for (i in composition[name]) {
+                    if (obj[i] === undefined) {
+                        throw new Error(
+                            '#' + name + ' - Unknown method:' + i
+                        )
+                    }
+                    aspects[name](
+                          composition[name][i]
+                        , obj
+                        , i
+                    )
+                }
+            }
+        }
+        processAspects('before')
+        processAspects('after')
+        processAspects('around')
+
+        function _create(args) {
+            var f
+            if (composition.singleton) {
+                return obj
+            } else {
+                f = function() {
+                    if (composition.create) {
+                        composition.create.apply(this, args)
+                    }
+                }
+                f.prototype = obj
+                return new f
+            }
+        }
+        if (composition.name) {
+            assemblies[composition.name] = _create
+        }
+        // TODO: forward extra args to _create
+        return _create()
+    }
+
+    function create(name) {
+        if (assemblies.hasOwnProperty(name)) {
+            var args = toArray(arguments)
+            if (args.length > 1) args.shift()
+            return assemblies[name](args)
+        }
+        else throw new Error('No assembly registered for name: ' + name)
+    }
+
+    // Export API
+    context.copper = {
+          compose: compose
+        , create: create
+        , before: before
+        , after: after
+        , around: around
+        , mixin: mixin
+    }
+})(this)
